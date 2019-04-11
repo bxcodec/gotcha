b34c6d74413987d4c2c70d5cd78cee9d1db3d8de
Iman Tumorang <bxcodec@gmail.com>
2019-04-11 19:05:47 +0700

    add item struct

--- lfu/repository/repository.go
+++ lfu/repository/repository.go
@@ -12,8 +12,43 @@ type Repository struct {
 	byKey         map[string]*list.Element
 }
 
+type cacheItem struct {
+	doc      *cache.Document
+	freqHead *list.Element
+}
+
+type frequencyItem struct {
+	entries   map[string]bool
+	frequency int
+}
+
 // Get ...
 func (r *Repository) Get(key string) (res *cache.Document, err error) {
-	panic("TODO: (bxcodec)")
+	elem, ok := r.byKey[key]
+	if elem == nil || !ok {
+		err = cache.ErrMissed
+		return
+	}
+	r.addFrequency(elem)
+	res = elem.Value.(*cache.Document)
+	return
+}
+
+func (r *Repository) addFrequency(elem *list.Element) {
+	// TODO: (bxcodec)
+}
+
+// Set ...
+func (r *Repository) Set(doc *cache.Document) (err error) {
+	// Check for existing item
+	if elem, ok := r.byKey[doc.Key]; ok {
+		elem.Value = doc
+		return nil
+	}
+
+	// elem := r.fragmentPositionList.PushFront(doc)
+	// r.items[doc.Key] = elem
+
+	panic("TODO")
 	return
 }
