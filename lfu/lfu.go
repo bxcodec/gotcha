d90fd5bb8179aeab073aaba799ee1c85c2d66a89
Iman Tumorang <bxcodec@gmail.com>
2019-04-11 14:23:08 +0700

    add TODO

--- lfu/lfu.go
+++ lfu/lfu.go
@@ -1,5 +1,51 @@
 package lfu
 
+import (
+	"sync"
+
+	"github.com/bxcodec/gotcha/cache"
+)
+
 // Repository ...
 type Repository interface {
+}
+
+// NewCache return the implementations of cache with LRU algorithm
+func NewCache(option cache.Option) cache.Interactor {
+
+	return &Cache{
+		Option: option,
+	}
+}
+
+// Cache ...
+type Cache struct {
+	sync.RWMutex
+	repo   Repository
+	Option cache.Option
+}
+
+// Set ...
+func (c *Cache) Set(key string, value interface{}) error {
+	panic("TODO: (bxcodec)")
+}
+
+// Get ...
+func (c *Cache) Get(key string) (val interface{}, err error) {
+	panic("TODO: (bxcodec)")
+}
+
+// Delete ...
+func (c *Cache) Delete(key string) (err error) {
+	panic("TODO: (bxcodec)")
+}
+
+// GetKeys ...
+func (c *Cache) GetKeys() (keys []string, err error) {
+	panic("TODO: (bxcodec)")
+}
+
+// ClearCache ...
+func (c *Cache) ClearCache() (err error) {
+	panic("TODO: (bxcodec)")
 }
