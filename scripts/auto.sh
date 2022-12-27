08dc9d763b3c09bd7f28b916c52c7bef3780eed4
Hiro <goferHiro@gmail.com>
2022-12-28 04:49:09 +0530

    :hammer: better automation for releases than make file
    Signed-off-by: Hiro <goferHiro@gmail.com>

---
+++ scripts/auto.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo "updating repo"
+
+git pull
+
+go work sync
+
+git describe --tags
+
+echo "what's the tag"
+
+read tag
+
+tag=$(echo "$tag" | sed 's/ //g')
+
+export TAG=$tag
+
+chmod +x scripts/release.sh
+chmod +x scripts/tag.sh
+
+./scripts/release.sh
+./scripts/tag.sh
+
+git add .
+git commit -m "final deps-$tag"
+
+git push
+git push origin --tags
+
+
+echo "verify the latest versionx"
+
+go list -m -json github.com/bxcodec/gotcha@latest
+go list -m -json github.com/bxcodec/gotcha/examples/basic@latest
