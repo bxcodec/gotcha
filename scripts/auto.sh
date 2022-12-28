94af253414186f35e4b74d604edeb54d7f0af70f
Hiro <goferHiro@gmail.com>
2022-12-28 10:53:32 +0000

    scripts/auto.sh

--- scripts/auto.sh
+++ scripts/auto.sh
@@ -28,7 +28,11 @@ git commit -m "chore: update deps-$tag"
 git push origin --tags
 git push origin release/$TAG
 
-echo "verify the latest versionx"
+echo "check the latest version, sleeping for 1 min for the go registry to sync up"
+
+sleep 1
 
 go list -m -json github.com/bxcodec/gotcha@latest
 go list -m -json github.com/bxcodec/gotcha/examples/basic@latest
+
+go run github.com/bxcodec/gotcha/examples/basic@$tag
